a201547e2ce94ef3a574457faa41c723d23d8433
Nevan Scott <nfscott@gmail.com>
2025-04-08 23:06:36 +0200

    group props in Widget component for better organization and clarity

--- src/components/Widget.svelte
+++ src/components/Widget.svelte
@@ -5,6 +5,14 @@
   $: style = props
     .map(({ name, value, unit }) => `--${name}: ${value}${unit};`)
     .join("");
+
+  $: grouped_props = Object.values(
+    props.reduce((acc, prop) => {
+      if (!acc[prop.group]) acc[prop.group] = { group: prop.group, props: [] };
+      acc[prop.group].props.push(prop);
+      return acc;
+    }, {})
+  );
 </script>
 
 <div class="exercise">
@@ -13,8 +21,21 @@
   </div>
 
   <div class="controls">
-    {#each props as { value, min, max, step = 1, unit = "", name, label = name }}
-      <Range bind:value {min} {max} {step} {name} {unit} {label} />
+    {#each grouped_props as { group, props }}
+      {#if group}
+        <details open class="group">
+          <summary>{group}</summary>
+          <div class="controls-layout">
+            {#each props as { value, min, max, step = 1, unit = "", name, label = name }}
+              <Range bind:value {min} {max} {step} {name} {unit} {label} />
+            {/each}
+          </div>
+        </details>
+      {:else}
+        {#each props as { value, min, max, step = 1, unit = "", name, label = name }}
+          <Range bind:value {min} {max} {step} {name} {unit} {label} />
+        {/each}
+      {/if}
     {/each}
   </div>
 </div>
@@ -36,14 +57,41 @@
     align-items: center;
     background-color: #191919;
   }
-  .controls {
+  .controls,
+  .controls summary {
     border-block-start: 1px solid #fff1;
+  }
+  .controls summary {
+    margin-top: -1px;
+  }
+  .controls,
+  .controls-layout {
     display: grid;
     grid-template-columns: max-content 1fr max-content;
     grid-auto-rows: min-content;
     gap: 2em 1.5em;
     padding: 2em;
     align-items: center;
+  }
+  .controls-layout {
+    padding-block: 0.5em 1.5em;
+    gap: 1em 1.5em;
+  }
+  .controls details {
+    grid-column: 1 / -1;
+    margin: -2em -2em 0;
+  }
+  .controls summary {
+    color: #888;
+    cursor: pointer;
+    font-size: smaller;
+    font-weight: bold;
+    letter-spacing: 0.05em;
+    padding: 1.5em 1rem 1.5em;
+    text-transform: uppercase;
+    -webkit-user-select: none; /* Safari */
+    -ms-user-select: none; /* IE 10+ */
+    user-select: none; /* Standard */
   }
   @media screen and (min-width: 1024px) {
     .exercise {
